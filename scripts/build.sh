5b7da24966118209509984553ff528cffb76fb8d
VardaanK-dev <vardaan.vaibhav@outlook.com>
2025-04-18 19:29:26 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y live-build
 
+mkdir liveos && cd liveos
+
+lb config \
+  --debian-installer live \
+  --distribution bookworm \
+  --archive-areas "main contrib non-free"
+
+lb build
